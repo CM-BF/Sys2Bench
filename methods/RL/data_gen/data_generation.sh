1ea1dda7a9692ce73bcf915825a77b7a9834f014
Citrine Gui <citrinegui@gmail.com>
2025-03-21 15:09:42 -0500

    [configure] countdown multiple dataset merging

---
+++ methods/RL/data_gen/data_generation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+python countdown.py --num_operands=4 --max_target=100
+python countdown.py --num_operands=4 --max_target=1000
+python countdown.py --num_operands=6 --max_target=100
+python countdown.py --num_operands=6 --max_target=1000
